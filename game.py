cd9e5c241170eea6c4cb5c954ad0d8163b7993d7
Krawett <elmat84@hotmail.fr>
2016-10-06 15:45:54 +0200

    Update game.py

--- game.py
+++ game.py
@@ -9,6 +9,7 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
+import os
 import pygame
 from random import randint
 
@@ -43,12 +44,14 @@ player = pygame.Rect(playerPosX, playerPosY,playerW, playerH)
 
 #Monsters
 
-monsterPosX = 500
-monsterPosY = 200
+monsterPosX = 350
+monsterPosY = 350
 monsterW = 90
 monsterH = 90
+monsterDestX = 350
+monsterDestY = 350
 
-monster = pygame.Rect(100, 100, monsterW, monsterH)
+monster = pygame.Rect(monsterPosX, monsterPosY, monsterW, monsterH)
 
 #===============================================================================
 #Creation Fenetre===============================================================
@@ -65,34 +68,35 @@ clock = pygame.time.Clock()
 
 while not gameOver:
 #EVENEMENTS=====================================================================
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            gameOver = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w:#HAUT
-                playerMoveUp = True
-            if event.key == pygame.K_s:#BAS
-                playerMoveDown = True
-            if event.key == pygame.K_a:#GAUCHE
-                playerMoveLeft = True
-            if event.key == pygame.K_d:#DROITE
-                playerMoveRight = True
-            if event.key == pygame.K_ESCAPE:#ECHAP | Quitte le jeu
+    if onGame:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 gameOver = True
-            if event.key == pygame.K_LSHIFT:#ECHAP | Quitte le jeu
-                playerRun = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:#HAUT
+                    playerMoveUp = True
+                if event.key == pygame.K_s:#BAS
+                    playerMoveDown = True
+                if event.key == pygame.K_a:#GAUCHE
+                    playerMoveLeft = True
+                if event.key == pygame.K_d:#DROITE
+                    playerMoveRight = True
+                if event.key == pygame.K_ESCAPE:#ECHAP | Quitte le jeu
+                    gameOver = True
+                if event.key == pygame.K_LSHIFT:#ECHAP | Quitte le jeu
+                    playerRun = True
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_w:#HAUT
-                playerMoveUp = False
-            if event.key == pygame.K_s:#BAS
-                playerMoveDown = False
-            if event.key == pygame.K_a:#GAUCHE
-                playerMoveLeft = False
-            if event.key == pygame.K_d:#DROITE
-                playerMoveRight = False
-            if event.key == pygame.K_LSHIFT:#ECHAP | Quitte le jeu
-                playerRun = False
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_w:#HAUT
+                    playerMoveUp = False
+                if event.key == pygame.K_s:#BAS
+                    playerMoveDown = False
+                if event.key == pygame.K_a:#GAUCHE
+                    playerMoveLeft = False
+                if event.key == pygame.K_d:#DROITE
+                    playerMoveRight = False
+                if event.key == pygame.K_LSHIFT:#ECHAP | Quitte le jeu
+                    playerRun = False
 
 #UPDATE=========================================================================
 
@@ -117,7 +121,26 @@ while not gameOver:
         else:
             player.move_ip(+playerVelocity, 0)#==============================
 
-    monster.move_ip(0, 1)
+    #Deplacement monstre
+    if monster.x < monsterDestX:
+        monster.move_ip(1, 0)
+    if monster.x > monsterDestX:
+        monster.move_ip(-1, 0)
+    if monster.y < monsterDestY:
+        monster.move_ip(0, 1)
+    if monster.y > monsterDestY:
+        monster.move_ip(0, -1)
+    elif monster.x == monsterDestX and monster.y == monsterDestY:
+        monsterDestX = randint(350, 510)
+        monsterDestY = randint(350, 510)
+
+    #Affichage position monstre
+    print()
+    print("Destination: ",monsterDestX, " | ",monsterDestY )
+    print("Position: ", monster.x, " | ", monster.y)
+
+
+
 #DRAW===========================================================================
     game.fill((255,255,255))
     pygame.draw.rect(game, (255,0,0), player, 1)
